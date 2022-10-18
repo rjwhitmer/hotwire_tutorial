a4051ce51d2aa1425b38cb90a072308bc3155acd
Bob <rjwhitmercc@gmail.com>
2022-10-18 16:48:14 -0600

    Finish line items dates data model

--- app/models/line_item_date.rb
+++ app/models/line_item_date.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
 class LineItemDate < ApplicationRecord
   belongs_to :quote
+
+  validates :date, presence: true, uniqueness: { scope: :quote_id }
+
+  scope :ordered, -> { order(date: :asc) }
 end
