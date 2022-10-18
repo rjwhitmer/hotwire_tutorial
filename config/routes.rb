a676a7d91d8c05d4e39c6efc22ecc3ef4a995fee
Bob <rjwhitmercc@gmail.com>
2022-10-18 16:55:18 -0600

    Add view and style for line item dates

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :quotes
+  resources :quotes do
+    resources :line_item_dates, except: [:index, :show]
+  end
 end
