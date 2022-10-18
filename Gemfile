2f3330e807020b5641704b89ed7600b33de296d8
Bob <rjwhitmercc@gmail.com>
2022-10-18 14:39:44 -0600

    Add devise auth

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '3.1.1'
 
+gem 'devise', '~> 4.8.1'
 gem 'simple_form', '~> 5.1.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -5,5 +5,5 @@ class Quote < ApplicationRecord
 
   scope :ordered, -> { order(id: :desc) }
 
-  after_create_commit -> { broadcast_prepend_to 'quotes', partial: 'quotes/quote', locals: { quote: self }, target: 'quotes' }
+  broadcasts_to ->(quote) { 'quotes' }, inserts_by: :prepend
 end
