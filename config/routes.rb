2c05de2f46df681d5d0c7151360ff4bbaa376029
Bob <rjwhitmercc@gmail.com>
2022-10-20 11:25:08 -0600

    Create routes and dummy data

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   resources :quotes do
-    resources :line_item_dates, except: [:index, :show]
+    resources :line_item_dates, except: %i[index show] do
+      resources :line_items, except: %i[index show]
+    end
   end
 end
